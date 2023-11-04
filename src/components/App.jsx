e5327967b8b873fc4ce03a5dc8555d6134ecc188
Kseniia111 <kseniya.markovych@gmail.com>
2023-11-04 19:18:38 +0200

    adding first contact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import { selectErrorContacts, selectisLoadingContacts } from 'redux/Selectors';
+import {
+  selectErrorContacts,
+  selectisLoadingContacts,
+  selectContacts,
+} from 'redux/Selectors';
 import { RotatingLines } from 'react-loader-spinner';
 import css from './App.module.css';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const contactsItems = useSelector(selectContacts);
   const isLoading = useSelector(selectisLoadingContacts);
   const error = useSelector(selectErrorContacts);
 
@@ -22,18 +27,24 @@ export const App = () => {
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-      <Filter />
-      {isLoading && !error && (
-        <RotatingLines
-          strokeColor="#e15b64"
-          strokeWidth="5"
-          animationDuration="0.75"
-          width="96"
-          visible={true}
-          ariaLabel="rotating-lines-loading"
-        />
+      {contactsItems.length !== 0 ? (
+        <>
+          <Filter />
+          {isLoading && !error && (
+            <RotatingLines
+              strokeColor="#e15b64"
+              strokeWidth="5"
+              animationDuration="0.75"
+              width="96"
+              visible={true}
+              ariaLabel="rotating-lines-loading"
+            />
+          )}
+          <ContactList />
+        </>
+      ) : (
+        <h3>Please add your first contact</h3>
       )}
-      <ContactList />
     </div>
   );
 };
